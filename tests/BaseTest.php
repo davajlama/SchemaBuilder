7376138c91d286602541742e7b5c5a0390811b45
David Bittner <david.bittner@4every1.cz>
2016-12-07 17:17:57 +0100

    Prepare env for travis tests

---
+++ tests/BaseTest.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Description of BaseTestCase
+ *
+ * @author David Bittner <david.bittner@seznam.cz>
+ */
+class BaseTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testBase()
+    {
+        $adapter    = new FakeAdapter();
+        $driver     = new Davajlama\SchemaBuilder\Driver\MySqlDriver($adapter);
+        $builder    = new Davajlama\SchemaBuilder\SchemaBuilder($driver);
+
+        $this->assertEquals(true, true);
+        $this->assertEquals(true, true);
+    }
+
+}
+
+class FakeAdapter implements \Davajlama\SchemaBuilder\Adapter\AdapterInterface
+{
+    public function fetchAll($sql)
+    {
+
+    }
+
+    public function query($sql)
+    {
+
+    }
+
+}
