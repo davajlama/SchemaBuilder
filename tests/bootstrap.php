96e4e36a36fcf9672b4a1a074c9ca499367c8804
David Bittner <david.bittner@seznam.cz>
2016-12-13 22:16:28 +0100

    Fix for mysql

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,6 +2,10 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+if(file_exists(__DIR__ . '/env.php')) {
+    require_once __DIR__ . '/env.php';
+}
+
 $loader = new Composer\Autoload\ClassLoader();
 $loader->addPsr4('Davajlama\\SchemaBuilder\\Test\\', [__DIR__]);
 $loader->register();
